9ac2ba1bf131ac11398b2439723104e55033541e
info@coders.VN <CodersTokyo>
2022-02-09 23:53:10 +0700

    push project

---
+++ Document.java
@@ -0,0 +1,16 @@
+public class Document {
+    private String string;
+
+    public String getString() {
+        return string;
+    }
+
+    public void setString(String string) {
+        this.string = string;
+    }
+
+    public String fix(String string){
+        string.trim();
+        return " ";
+    }
+}
---
+++ Manage.java
@@ -0,0 +1,3 @@
+public class Manage {
+    Document document = new Document();
+}
